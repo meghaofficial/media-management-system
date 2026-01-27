fee825004d330efbd3abd4c7dd672790bc06e927
meghasharma <meghaofficial789@gmail.com>
2026-01-28 00:14:51 +0530

    temp commit 2

--- frontend/src/components/FolderCard.tsx
+++ frontend/src/components/FolderCard.tsx
@@ -4,22 +4,22 @@ import { useState } from "react";
 import Popup from "../ReusableComponents/Popup";
 
 type FolderCardType = {
-  activeFolder: FolderItem | null;
-  setActiveFolder: React.Dispatch<React.SetStateAction<FolderItem | null>>;
-  id: string;
+  details: FolderItem;
+  activeFolderID: string | number | null;
+  setActiveFolderID: React.Dispatch<React.SetStateAction<string | number | null>>;
 };
 
-export default function FolderCard({ activeFolder, setActiveFolder, id } : FolderCardType) {
+export default function FolderCard({ details, activeFolderID, setActiveFolderID }: FolderCardType) {
 
   const [editFolder, setEditFolder] = useState<string>("");
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
 
   return (
     <>
-      {activeFolder?.id?.toString() === id ? (
+      {activeFolderID === details?.id ? (
         <div className="cursor-pointer">
-          <svg  
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 640 640"
             fill="currentColor"
@@ -29,7 +29,10 @@ export default function FolderCard({ activeFolder, setActiveFolder, id } : Folde
           </svg>
         </div>
       ) : (
-        <div className="cursor-pointer relative" onClick={() => setActiveFolder(null)}>
+        <div className="cursor-pointer relative" onClick={() => setActiveFolderID(null)}>
+          {details.icon && (
+            <img src={details?.icon} alt="" className="rounded-full h-8 w-8 object-cover absolute top-4.5 left-4" />
+          )}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 640 640"
@@ -38,29 +41,25 @@ export default function FolderCard({ activeFolder, setActiveFolder, id } : Folde
           >
             <path d="M128 512L512 512C547.3 512 576 483.3 576 448L576 208C576 172.7 547.3 144 512 144L362.7 144C355.8 144 349 141.8 343.5 137.6L305.1 108.8C294 100.5 280.5 96 266.7 96L128 96C92.7 96 64 124.7 64 160L64 448C64 483.3 92.7 512 128 512z" />
           </svg>
-          <p className="absolute bottom-6 left-4.5 text-[14px] text-gray-500">3 Files</p>
+          <p className="absolute bottom-6 right-4.5 text-[14px] text-gray-500">{details?.no_of_images} Files</p>
           {/* menu */}
-          <div className={`absolute top-7 right-4 text-gray-500 p-1`} onClick={(e) => {
+          {/* <div className={`absolute top-7 right-4 text-gray-500 p-1`} onClick={(e) => {
             e.stopPropagation();
             setEditFolder(id);
             setOpen(true);
             // alert("hi")
           }}>
             <BsThreeDotsVertical size={12} />
-          </div>
+          </div> */}
         </div>
       )}
 
-      {/* <button
-        onClick={() => setOpen(true)}
-        className="rounded bg-blue-600 px-4 py-2 text-white"
-      >
-        Open Popup
-      </button> */}
+      <p className="relative -top-3.5 left-3 line-clamp-1 max-w-25 text-[14px]">{details?.name}</p>
+      <p className="relative -top-3.5 left-3 line-clamp-1 max-w-25 text-[12px] text-gray-500">({details?.size})</p>
 
-      <Popup open={open} onClose={() => setOpen(false)}>
+      {/* <Popup open={open} onClose={() => setOpen(false)}>
         <img src="https://static.vecteezy.com/system/resources/thumbnails/005/346/410/small/close-up-portrait-of-smiling-handsome-young-caucasian-man-face-looking-at-camera-on-isolated-light-gray-studio-background-photo.jpg" className="rounded-full h-10 w-10 object-cover" alt="" />
-      </Popup>
+      </Popup> */}
     </>
   )
 }
--- frontend/src/panels/FoldersPanel.tsx
+++ frontend/src/panels/FoldersPanel.tsx
@@ -5,30 +5,34 @@ export default function FoldersPanel() {
 
   const [activeFolder, setActiveFolder] = useState<FolderItem | null>(null);
   const [foldersList, setFoldersList] = useState<FolderItem[]>([
-    { id: 1, name: "Folder 1", no_of_images: 3, icon: "", size: "5KB" },
-    { id: 2, name: "Folder 2", no_of_images: 5, icon: "", size: "10MB" },
-    { id: 3, name: "Folder 3", no_of_images: 10, icon: "", size: "50KB" },
+    { id: 1, name: "Folder 1", no_of_images: 3, icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Young_girl_smiling_in_sunshine_%282%29.jpg/960px-Young_girl_smiling_in_sunshine_%282%29.jpg", size: "5KB" },
+    { id: 2, name: "Folder 2", no_of_images: 5, icon: "https://i0.wp.com/pixahive.com/wp-content/uploads/2021/02/An-Indian-boy-375075-pixahive.jpg?fit=1702%2C2560&ssl=1", size: "10MB" },
+    { id: 3, name: "Folder 3", no_of_images: 10, icon: "https://img.freepik.com/free-photo/close-up-portrait-handsome-smiling-young-man-white-t-shirt-blurry-outdoor-nature_176420-6305.jpg?semt=ais_user_personalization&w=740&q=80", size: "50KB" },
     { id: 4, name: "Folder 4", no_of_images: 3, icon: "", size: "50MB" },
     { id: 5, name: "Folder 5", no_of_images: 16, icon: "", size: "0KB" },
     { id: 6, name: "Folder 6", no_of_images: 7, icon: "", size: "50KB" },
   ]);
 
-  // const addFolder = () => {
-  //   console.log("Test")
-  //   setFoldersList(prev => ([ ...prev, { id: foldersList.length+1, name: `Folder ${foldersList.length+1}`, no_of_images: 4, icon: "" } ]));
-  // }
+  const [activeFolderID, setActiveFolderID] = useState<string | number | null>("");
+
+  const addFolder = () => {
+    setFoldersList(prev => ([ ...prev, { id: foldersList.length+1, name: `Folder ${foldersList.length+1}`, no_of_images: 0, icon: "", size: "0KB" } ]));
+  }
 
   return (
     <>
       <div className="flex w-full items-center justify-center">
-        <div className="flex flex-wrap justify-center gap-x-5 gap-y-3">
+        <div className="flex flex-wrap justify-center gap-x-5">
           {foldersList?.map((folder, index) => (
-            <div key={index} onClick={() => setActiveFolder(prev => prev ? null : folder)}>
+            <div key={index} 
+            // onClick={() => setActiveFolder(prev => prev ? null : folder)}
+            onClick={() => setActiveFolderID(prev => prev ? null : folder?.id)}
+            >
               <FolderCard
                 key={index}
-                activeFolder={activeFolder}
-                setActiveFolder={setActiveFolder}
-                id={folder?.id.toString()}
+                details={folder}
+                activeFolderID={activeFolderID}
+                setActiveFolderID={setActiveFolderID}
               />
             </div>
           ))}
